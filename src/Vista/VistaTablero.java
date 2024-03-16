d1bcaf8d90a79da2c38fae192d9b84588f85e5aa
JuanDIego11-stack <jgarcia.7234@unimar.edu.ve>
2024-03-16 16:59:25 -0400

    se añadio un menu principal

--- src/Vista/VistaTablero.java
+++ src/Vista/VistaTablero.java
@@ -7,10 +7,7 @@ package Vista;
 
 import Controlador.Controlador;
 
-/**
- *
- * @author Dani
- */
+
 public class VistaTablero extends javax.swing.JFrame {
 
     /**
@@ -18,9 +15,9 @@ public class VistaTablero extends javax.swing.JFrame {
      */
     public VistaTablero() {
         initComponents();
-        //this.setSize(900, 920);
         this.setLocationRelativeTo(null);
-        this.setTitle("Ajedrez");
+        this.setTitle("Talbero Vs CPU");
+        this.dispose();
     }
 
     /**
@@ -102,6 +99,7 @@ public class VistaTablero extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -478,13 +476,21 @@ public class VistaTablero extends javax.swing.JFrame {
 
         jMenu2.setText("Exit");
 
-        jMenuItem2.setText("Aceptar");
+        jMenuItem2.setText("Cerrar Juego");
         jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem2ActionPerformed(evt);
             }
         });
         jMenu2.add(jMenuItem2);
+
+        jMenuItem3.setText("ir al menu");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem3);
 
         jMenuBar1.add(jMenu2);
 
@@ -516,6 +522,12 @@ public class VistaTablero extends javax.swing.JFrame {
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         System.exit(0);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        this.setVisible(false);
+        menuPrincipal menu = new  menuPrincipal();
+        menu.setVisible(true);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -622,6 +634,7 @@ public class VistaTablero extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
--- src/Vista/menuPrincipal.java
+++ src/Vista/menuPrincipal.java
@@ -4,17 +4,14 @@
  */
 package Vista;
 
-/**
- *
- * @author juans
- */
+
 public class menuPrincipal extends javax.swing.JFrame {
 
-    /**
-     * Creates new form menuPrincipal
-     */
+ 
     public menuPrincipal() {
         initComponents();
+        this.setLocationRelativeTo(null);
+        this.setTitle("Menu Principalñ");
     }
 
     /**
@@ -26,21 +23,76 @@ public class menuPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        botonContraBOt = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(38, 37, 34));
+
+        jLabel1.setBackground(new java.awt.Color(38, 37, 34));
+        jLabel1.setFont(new java.awt.Font("Dialog", 0, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Ajedrez");
+
+        botonContraBOt.setBackground(new java.awt.Color(115, 149, 82));
+        botonContraBOt.setForeground(new java.awt.Color(255, 255, 255));
+        botonContraBOt.setText("Nueva Partida Contra CPU");
+        botonContraBOt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonContraBOtActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(261, 261, 261)
+                        .addComponent(botonContraBOt, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(434, 434, 434)
+                        .addComponent(jLabel1)))
+                .addContainerGap(437, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addComponent(jLabel1)
+                .addGap(87, 87, 87)
+                .addComponent(botonContraBOt, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(325, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 901, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 549, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void botonContraBOtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonContraBOtActionPerformed
+       this.setVisible(false);
+       VistaTablero tablero = new VistaTablero();
+       tablero.setVisible(true);
+       this.dispose();
+    }//GEN-LAST:event_botonContraBOtActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +130,8 @@ public class menuPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botonContraBOt;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
